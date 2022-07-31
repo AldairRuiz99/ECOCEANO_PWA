8544e7035c3051dd22df8d841909f377ccaac18f
aldair@lania.mx <aldair.ruiz@lania>
2022-07-31 11:51:53 -0500

    Primer push al proyecto

---
+++ ECOCEANO/Data/EcoceanoContext.cs
@@ -0,0 +1,47 @@
+﻿using ECOCEANO.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOCEANO.Data
+{
+    public class EcoceanoContext : DbContext
+    {
+            public EcoceanoContext(DbContextOptions<EcoceanoContext> options) : base(options)
+            {
+
+            }
+            public DbSet<Usuarios> Usuario { get; set; }
+            public DbSet<Productos> Producto { get; set; }
+            public DbSet<Categorias> Categoria { get; set; }
+            public DbSet<Pedidos> Pedido { get; set; }
+            public DbSet<Ventas> Venta { get; set; }
+            public DbSet<DetallesVentas> DetalleVentas { get; set; }
+            public DbSet<DetallesPedidos> DetallePedidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Usuarios>()
+                .Property(b => b.ID).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Ventas>()
+                .Property(b => b.MontoTotal).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Productos>()
+                .Property(b => b.Precio).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<DetallesPedidos>()
+                .Property(b => b.MontoTotal).HasColumnType("decimal(18,2)");
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    {
+        //        //options.UseMySQL("Server=localhost;Port=3306;Database=ecoceanobd;User ID=root;Password=41dair;CharSet=utf8;");
+        //    }
+
+    }
+}
---
+++ ECOCEANO/Models/Usuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOCEANO.Models
+{
+    public class Usuarios
+    {
+        internal DateTime fechaRegistro;
+
+        public Usuarios()
+        {
+            // this.ID = 1;//
+            //this.nombre = "Juanito";
+            //this.pApellido = "Bananas";
+            //this.sApellido = "Zarate";
+            //this.correo = "juan.banana@gmail.com";
+            //this.password = "qwerty";
+            //this.rol = "user";
+            //this.estatus = true;
+            //Console.WriteLine("Usuario creado");
+
+        }
+        // Propiedades de la clase 
+        //Public tipoDeDato nombrePropiedad {get; set}
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public string pApellido { get; set; }
+        public string sApellido { get; set; }
+        public string correo { get; set; }
+        public string password { get; set; }
+        public DateTime fecharegistro { get; set; }
+        public string rol { get; set; }
+        public Boolean estatus { get; set; }
+
+    }
+}
